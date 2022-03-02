1944254cedb0c6f7f7ad4a70c86e0f07f16e7880
Brandon Clay <imagesofclay@gmail.com>
2022-03-02 17:54:00 -0600

    Initial commit of Term Scheduler

---
+++ app/src/main/java/android/TermScheduler/UI/TermList.java
@@ -0,0 +1,14 @@
+package android.TermScheduler.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.TermScheduler.R;
+public class TermList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_term_list);
+    }
+}
