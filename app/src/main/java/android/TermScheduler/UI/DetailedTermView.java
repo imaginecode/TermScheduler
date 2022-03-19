20a0c9605b2ce693a725de5ae058ef2f94ca8d35
Brandon Clay <imagesofclay@gmail.com>
2022-03-19 11:15:04 -0500

    Detailed Term View layout visually completed

--- app/src/main/java/android/TermScheduler/UI/DetailedTermView.java
+++ app/src/main/java/android/TermScheduler/UI/DetailedTermView.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.TermScheduler.R;
+import android.view.View;
+
 public class DetailedTermView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_term_view);
+    }
+
+    public void goToCourseList(View view) {
+        //need to add code to move to next view
+    }
+
+    public void saveButton(View view) {
+        //Add code to save and do update queries
     }
 }
